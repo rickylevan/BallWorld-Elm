3e43f2de9802906430164cf6a6c774d2b60756e1
Ricky LeVan <rick.levan@gmail.com>
2016-05-15 12:34:13 -0700

    ball collisions appear to work

--- BallWorld.elm
+++ BallWorld.elm
@@ -5,6 +5,7 @@ import Element
 import Collage
 import Html exposing (Html)
 import AnimationFrame
+import Math.Vector2 exposing (..)
 
 
 -- MODEL
@@ -12,41 +13,21 @@ import AnimationFrame
 (gameWidth, gameHeight) = (500, 500)
 (halfWidth, halfHeight) = (gameWidth/2, gameHeight/2)
 
-type alias Ball = 
-    { x : Float
-    , y : Float
-    , dx : Float
-    , dy : Float
-    , r : Float
-    , c : Color.Color
-    }
+type alias Ball =
+    { x : Float, y : Float, dx : Float, dy : Float
+    , r : Float, c : Color.Color}
 
-defaultBall1 = 
-    { x = 0
-    , y = 0
-    , dx = 84
-    , dy = 35
-    , r = 45
-    , c = lightBlue
-    }
+defaultBall1 =
+    { x = 0, y = 0, dx = 184, dy = 335
+    , r = 25, c = lightBlue}
 
-defaultBall2 = 
-    { x = 80
-    , y = 0
-    , dx = 4
-    , dy = 35
-    , r = 30
-    , c = lightRed
-    }
+defaultBall2 =
+    { x = 80, y = 0, dx = 200, dy = 0
+    , r = 30, c = lightRed}
 
-defaultBall3 = 
-    { x = -80
-    , y = 0
-    , dx = -100
-    , dy = 235
-    , r = 35
-    , c = lightGreen
-    }
+defaultBall3 =
+    { x = -80, y = 0, dx = -400, dy = 235
+    , r = 35, c = lightGreen}
 
 defaultBalls = [defaultBall1, defaultBall2, defaultBall3]
 
@@ -60,9 +41,9 @@ init =
 
 -- UPDATE
 
-updateBall : Time.Time -> Ball -> Ball
-updateBall delta ball = 
-    let 
+moveBounceBall : Time.Time -> Ball -> Ball
+moveBounceBall delta ball =
+    let
         rightBound = ball.x + ball.r
         leftBound = ball.x - ball.r
         topBound = ball.y + ball.r
@@ -74,12 +55,16 @@ updateBall delta ball =
         xVelFlip = if (rightFlab > 0 || leftFlab < 0) then -1 else 1
         yVelFlip = if (topFlab > 0 || bottomFlab < 0) then -1 else 1
     in
-        {ball | 
+        {ball |
         x = ball.x + xVelFlip*ball.dx*delta - 2*(rightFlab+leftFlab),
         y = ball.y + yVelFlip*ball.dy*delta - 2*(topFlab+bottomFlab),
         dx = xVelFlip * ball.dx,
         dy = yVelFlip * ball.dy
         }
+
+moveBounceBalls : Time.Time -> List Ball -> List Ball
+moveBounceBalls delta ballList =
+    List.map (moveBounceBall delta) ballList
 
 
 type Msg
@@ -88,15 +73,15 @@ type Msg
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Tick delta -> ((List.map (updateBall delta) model), Cmd.none)
-
-
+    Tick delta ->
+        let moveBounce = moveBounceBalls delta
+        in (model |> moveBounce |> collideBalls, Cmd.none)
 
 -- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
-subscriptions = (\_ -> Sub.batch 
+subscriptions = (\_ -> Sub.batch
         [AnimationFrame.diffs (Tick<<inSeconds)])
 
 
@@ -110,14 +95,14 @@ lightGray = Color.rgb 200 200 200
 darkBlue = Color.rgb 0 0 50
 
 viewBalls : List Ball -> List Collage.Form
-viewBalls ballList = 
+viewBalls ballList =
     let f ball = Collage.circle ball.r
         |> Collage.filled ball.c
         |> Collage.move (ball.x, ball.y)
     in List.map f ballList
 
 view : Model -> Html Msg
-view model = 
+view model =
     Element.toHtml (Element.container 500 500 Element.middle <|
     Collage.collage gameWidth gameHeight
     ([ Collage.rect gameWidth gameHeight
@@ -134,3 +119,81 @@ main =
     , update = update
     , subscriptions = subscriptions
     }
+
+-- PHYSICS
+
+mass : Ball -> Float
+mass b = b.r ^ 2
+
+distance : Ball -> Ball -> Float
+distance b0 b1 =
+    let vec0 = vec2 b0.x b0.y
+        vec1 = vec2 b1.x b1.y
+    in
+        (sub vec0 vec1) |> length
+
+
+collisionTime : Ball -> Ball -> Float
+collisionTime b0 b1 =
+    let dvel = deltaVel b0 b1
+        dpos = deltaPos b0 b1
+        r = b0.r + b1.r
+        phys = ((dot dvel dpos) ^ 2) -
+            (dot dvel dvel) *
+            ((dot dpos dpos) - (r ^ 2))
+    in ((neg (dot dvel dpos)) + (neg (sqrt phys))) /
+        (dot dvel dvel)
+
+deltaVel : Ball -> Ball -> Vec2
+deltaVel b0 b1 =
+    vec2 (b1.dx - b0.dx) (b1.dy - b0.dy)
+
+deltaPos : Ball -> Ball -> Vec2
+deltaPos b0 b1 =
+    vec2 (b1.x - b0.x) (b1.y - b0.y)
+
+neg : number -> number
+neg x = -x
+
+overlapping : Ball -> Ball -> Bool
+overlapping b0 b1 =
+    (distance b0 b1) < b0.r + b1.r
+
+nudge = 1.1
+
+reducedMass : Ball -> Ball -> Float
+reducedMass b0 b1 =
+    let m0 = mass b0
+        m1 = mass b1
+    in (m0 * m1) / (m0 + m1)
+
+impulse : Ball -> Ball -> Vec2
+impulse b0 b1 =
+    let dist = (distance b0 b1)
+        nx = (b1.x - b0.x) / dist
+        ny = (b1.y - b0.y) / dist
+        dvn = (nx * (b1.dx - b0.dx)) +
+              (ny * (b1.dy - b0.dy))
+        rm = reducedMass b0 b1
+    in vec2 (2 * rm * dvn * nx) (2 * rm * dvn * ny)
+
+
+collideBalls : List Ball -> List Ball
+collideBalls ballList =
+    let collideWithOthers subList b =
+        case subList of
+            x :: xs ->
+                if b /= x && overlapping b x then collide b x
+                else collideWithOthers xs b
+            [] -> b
+    in List.map (collideWithOthers ballList) ballList
+
+-- Update b0 in collision with perfectly fixed b1
+collide : Ball -> Ball -> Ball
+collide b0 b1 =
+    let dxNext = b0.dx + (((impulse b0 b1) |> getX) / (mass b0))
+        dyNext = b0.dy + (((impulse b0 b1) |> getY) / (mass b0))
+        tstar = collisionTime b0 b1
+        xNext = b0.x + tstar*b0.dx + neg (nudge * tstar * dxNext)
+        yNext = b0.y + tstar*b0.dy + neg (nudge * tstar * dyNext)
+    in { b0 | x = xNext, y = yNext, dx = dxNext, dy = dyNext}
